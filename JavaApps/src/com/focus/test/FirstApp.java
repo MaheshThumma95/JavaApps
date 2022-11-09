94e760e85d3aa5b8e7da52a2bb695b1f63906a77
mahesh <thumma.mahesh1995@gmail.com>
2022-11-09 18:13:53 +0530

    First Commit

---
+++ JavaApps/src/com/focus/test/FirstApp.java
@@ -0,0 +1,16 @@
+package com.focus.test;
+
+public class FirstApp {
+
+	public static void main(String[] args) {
+		int[] a = {10,20,30,40};
+		for(int elem : a) {
+			System.out.println(elem);
+		}
+		
+		String name = "sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The length of the String is:"+count);
+	}
+
+}
