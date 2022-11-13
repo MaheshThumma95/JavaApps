e49b3e0400269a515b0d82960c601a89dbc2404f
mahesh <thumma.mahesh1995@gmail.com>
2022-11-13 16:36:09 +0530

    Guesser game enhancements: 1)Guess Range b/w 1-100 2)Show Warning msg for player if player out of the range. 3)Ask Again the value if player enter out of the range.

--- JavaApps/src/com/guesser/game/GuesserGameMain.java
+++ JavaApps/src/com/guesser/game/GuesserGameMain.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 
 class Guesser {
 	int guessNum;
-
 	int guessNumber() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Guesser kindly guess the number");
+		System.out.println("Guesser kindly guess the number (1-100)");
 		guessNum = scan.nextInt();
 		return guessNum;
 	}
@@ -17,9 +16,16 @@ class Player {
 	int guessNum;
 
 	int guessNumber() {
-		System.out.println("Playerr kindly guess the number");
+		System.out.println("\nPlaye kindly guess the number (1-100)");
 		Scanner scan = new Scanner(System.in);
 		guessNum = scan.nextInt();
+
+		if (guessNum > 0 && guessNum <= 100) {
+			return guessNum;
+		} else {
+			System.out.println("Invalid Input, Please Enter Again between 1-100 only");
+			guessNum = scan.nextInt();
+		}
 		return guessNum;
 	}
 }
@@ -29,17 +35,18 @@ class Umpire {
 	int numFromPlayer1;
 	int numFromPlayer2;
 	int numFromPlayer3;
-
-	void collectNumFromGuesser() {
+	
+	int collectNumFromGuesser() {
 		Guesser g = new Guesser();
 		numFromGuesser = g.guessNumber();
+		return numFromGuesser;
 	}
 
 	void collectNumFromPlayers() {
 		Player p1 = new Player();
 		Player p2 = new Player();
 		Player p3 = new Player();
-
+		
 		numFromPlayer1 = p1.guessNumber();
 		numFromPlayer2 = p2.guessNumber();
 		numFromPlayer3 = p3.guessNumber();
@@ -48,7 +55,7 @@ class Umpire {
 	void compare() {
 		if (numFromGuesser == numFromPlayer1) {
 			if (numFromGuesser == numFromPlayer2 && numFromGuesser == numFromPlayer3) {
-				System.out.println("All Players Guess Correct Number::");
+				System.out.println("Congratulations!, All The Players Guess Correct");
 			} else if (numFromGuesser == numFromPlayer2) {
 				System.out.println("Player1 and Player 2 won the game");
 			} else if (numFromGuesser == numFromPlayer3) {
@@ -75,8 +82,15 @@ public class GuesserGameMain {
 	public static void main(String[] args) {
 
 		Umpire u = new Umpire();
-		u.collectNumFromGuesser();
-		u.collectNumFromPlayers();
-		u.compare();
+		int collectGuessNum = u.collectNumFromGuesser();
+
+		//check range for guesser number
+		if (collectGuessNum > 0 && collectGuessNum <= 100) {
+			u.collectNumFromPlayers();
+			u.compare();
+		} else {
+			System.out.println("Guesser, Plese select guess range b/w 1-100 only");
+		}
+
 	}
 }
